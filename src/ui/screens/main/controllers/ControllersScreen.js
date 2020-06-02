4b795f05589e7b9161874cb24396b1c675715c79
kaizaro <kaizaro78@gmail.com>
2020-06-03 01:56:12 +0300

    made controllers screen

--- src/ui/screens/main/controllers/ControllersScreen.js
+++ src/ui/screens/main/controllers/ControllersScreen.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import {Container} from '../../../components/Container';
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {APP_STYLES} from '../../../../helpers/styleguide';
 import {CONTROLLERS} from '../../../../constants/data/Controllers';
 import {ButtonTransparent} from '../../../components/buttons/ButtonTransparent';
@@ -13,6 +13,43 @@ import {
 import {ButtonRegular} from '../../../components/buttons/ButtonRegular';
 
 export const ControllersScreen: () => React$Node = () => {
+    // logic block
+    const onControllerPress = (): void => {
+        Alert.alert(
+            'Выберите действие',
+            'Вы хотите изменить конфигурацию контроллера?',
+            [
+                {
+                    text: 'Сброс контроллера',
+                    onPress: onDeleteButtonPress,
+                },
+                {
+                    text: 'Отмена',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                },
+                {text: 'OK', onPress: () => console.log('OK Pressed')}
+            ],
+            {cancelable: false},
+        );
+    };
+
+    const onDeleteButtonPress = (): void => {
+        Alert.alert(
+            'Подтверждение сброса контроллера',
+            'Вы точно хотите сбросить контроллер до заводских настроек?',
+            [
+                {
+                    text: 'Отменить',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                },
+                {text: 'Сбросить', onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+        );
+    };
+
     // render block
     return (
         <Container style={styles.container}>
@@ -27,19 +64,20 @@ export const ControllersScreen: () => React$Node = () => {
                 {CONTROLLERS.map(controller => {
                     console.log(controller);
                     return (
-                        <View style={styles.row}>
+                        <TouchableOpacity style={styles.row} onPress={onControllerPress}>
                             <Text
                                 style={{
                                     ...APP_STYLES.TEXT.LIST_ITEM,
                                     fontSize: scaleFontSize(18),
+                                    color: APP_STYLES.COLOR.BLACK,
                                 }}>
                                 {controller.name}
                             </Text>
-                            <ButtonTransparent
-                                style={styles.settingButton}
-                                title={'Настройка'}
-                            />
-                        </View>
+                            {/*<ButtonTransparent*/}
+                            {/*    style={styles.settingButton}*/}
+                            {/*    title={'Настройка'}*/}
+                            {/*/>*/}
+                        </TouchableOpacity>
                     );
                 })}
             </View>
@@ -64,17 +102,17 @@ const styles = StyleSheet.create({
         marginBottom: scaleVertical(20),
         width: '100%',
         paddingVertical: scaleVertical(10),
-        paddingHorizontal: scaleHorizontal(15),
+        paddingHorizontal: scaleHorizontal(25),
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        borderColor: APP_STYLES.COLOR.LIGHT_BLUE,
-        borderWidth: scaleHorizontal(1),
+        borderColor: APP_STYLES.COLOR.LIGHT_GRAY,
+        borderBottomWidth: scaleHorizontal(1),
         borderRadius: scaleHorizontal(10),
     },
-    settingButton: {
-        width: scaleHorizontal(100),
-        height: scaleVertical(25),
-        borderColor: APP_STYLES.COLOR.LIGHT_GRAY,
-    },
+    // settingButton: {
+    //     width: scaleHorizontal(100),
+    //     height: scaleVertical(25),
+    //     borderColor: APP_STYLES.COLOR.LIGHT_GRAY,
+    // },
 });
